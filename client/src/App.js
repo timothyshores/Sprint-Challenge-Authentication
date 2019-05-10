76f7f942b7215f64d7735a9ed7bc0de50c18d628
Timothy Shores <timothymshores@gmail.com>
2019-05-10 11:04:17 -0500

    Create client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, NavLink } from 'react-router-dom';
+
+import Login from './auth/Login';
+import SignUp from './auth/SignUp';
+import Jokes from './jokes/Jokes';
+import Reminder from './auth/Reminder';
+
 import './App.css';
 
-function App() {
+function App(props) {
+    const logout = () => {
+        localStorage.removeItem('authorization');
+        window.location.pathname = '/'
+    }
+
     return (
         <div className="App">
             <header className="App-header">
-
+                <h1>Sprint Challenge Authentication</h1>
+                <NavLink to="/jokes">Jokes</NavLink>
+                &nbsp; | &nbsp;
+                <NavLink to="/signup">Sign Up</NavLink>
+                &nbsp; | &nbsp;
+                <NavLink to="/login">Login</NavLink>
+                &nbsp; | &nbsp;
+                <a href="/" onClick={logout}>Logout</a>
             </header>
+            <main>
+                <Route path="/jokes" component={Jokes} />
+                <Route path="/signup" component={SignUp} />
+                <Route path="/login" component={Login} />
+                <Route exact path="/" component={Reminder} />
+            </main>
         </div>
     );
 }
