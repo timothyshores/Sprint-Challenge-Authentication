1bc247d45b985861b41fb90a9f88e2518f5690ab
Timothy Shores <timothymshores@gmail.com>
2019-05-10 10:14:55 -0500

    Create config/models.js

--- config/routes.js
+++ config/routes.js
@@ -3,30 +3,30 @@ const axios = require('axios');
 const { authenticate } = require('../auth/authenticate');
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+    server.post('/api/register', register);
+    server.post('/api/login', login);
+    server.get('/api/jokes', authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+    // implement user registration
 }
 
 function login(req, res) {
-  // implement user login
+    // implement user login
 }
 
 function getJokes(req, res) {
-  const requestOptions = {
-    headers: { accept: 'application/json' },
-  };
+    const requestOptions = {
+        headers: { accept: 'application/json' },
+    };
 
-  axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
-    .then(response => {
-      res.status(200).json(response.data.results);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+    axios
+        .get('https://icanhazdadjoke.com/search', requestOptions)
+        .then(response => {
+            res.status(200).json(response.data.results);
+        })
+        .catch(err => {
+            res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+        });
 }
